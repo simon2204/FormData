5db5cfd9e731ea79a9ac022d70f30864171bdb64
Simon <simon2204s@gmail.com>
2021-07-14 00:35:32 +0200

    downgraded package version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,7 +14,8 @@ let package = Package(
     targets: [
         .target(
             name: "FormData",
-            dependencies: []
+            dependencies: [],
+            exclude: ["FormData.docc"]
         ),
         .testTarget(
             name: "FormDataTests",
